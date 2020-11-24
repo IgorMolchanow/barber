5e4f2fcdacbad82bc45e8878b15698fd40d27ed6
IgorMolchanow <73214908+IgorMolchanow@users.noreply.github.com>
2020-11-24 15:45:37 +0300

    js

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
 var mapButton = document.querySelector(".contacts-button-map");
 var mapPopup = document.querySelector(".modal-map");
-var mapClosed = popup.querySelector(".modal-close");
+var mapClose = popup.querySelector(".modal-close");
 mapButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     mapPopup.classList.add("modal-show");
 });
-mapClosed.addEventListener("click", function (evt) {
+mapClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     mapPopup.classList.remove("modal-show");
 });
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
-var closed = popup.querySelector(".modal-close");
+var close = popup.querySelector(".modal-close");
 var form = popup.querySelector("form");
 var login = popup.querySelector("[name=login]");
 var password = popup.querySelector("[name=password]");
@@ -22,7 +22,7 @@ link.addEventListener("click", function (evt) {
         login.focus();
     }
 });
-closed.addEventListener("click", function (evt) {
+close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
     popup.classList.add("modal-error");
@@ -32,7 +32,7 @@ form.addEventListener("submit", function (evt) {
         evt.preventDefault();
         popup.classList.add("modal-error");
         popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal.error");
+        popup.classList.add("modal-error");
     } else {
         if (isStorageSupport) {
             localStorage.setItem("login", login.value);
